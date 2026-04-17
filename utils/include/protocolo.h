0961009a296ce6c49c85b7b1c7fb3ff52dceebf8
micaach <rchavezgonzalez@frba.utn.edu.ar>
2026-04-17 01:48:46 +0000

    Protocolo.h handchake para kernel memory

--- utils/include/protocolo.h
+++ utils/include/protocolo.h
@@ -9,7 +9,12 @@ typedef enum{
     SWAP_HANDSHAKE, // para el handshake inicial entre swap y kernel memory
     SWAP_REQUEST, // para enviar el tamaño de bloque y total a memoria
     CPU_KERNEL_MEMORY_HANDSHAKE,    
-    CPU_KERNEL_SCHEDULER_HANDSHAKE
+    CPU_KERNEL_SCHEDULER_HANDSHAKE,
+    //HANDSHAKE PARA KERNEL MEMORY Y EL MODULO CORRESPONDIENTE
+    KERNEL_SCHEDULER_HANDSHAKE,
+    MEMORY_STICK_HANDSHAKE,
+    CPU_HANDSHAKE
+
 }op_code;
 
 #endif /* PROTOCOLO_H_ */
