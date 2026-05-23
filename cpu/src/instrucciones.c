7c1b753279a055181a1013625dfad65ef0f5e2cf
yanisilesutn <ysilesmontano@frba.utn.edu.ar>
2026-05-23 20:37:24 -0300

    desarrollo las ejecución de las instrucciones

--- cpu/src/instrucciones.c
+++ cpu/src/instrucciones.c
@@ -3,6 +3,43 @@
 #include <stdlib.h>
 #include <unistd.h>
 
+
+bool es_registro_8bits(char* nombre_registro) {
+    return (strcmp(nombre_registro, "AX") == 0 || 
+            strcmp(nombre_registro, "BX") == 0 || 
+            strcmp(nombre_registro, "CX") == 0 || 
+            strcmp(nombre_registro, "DX") == 0);
+}
+
+void* obtener_registro(t_registros* registros, char* nombre_registro) {
+    if (strcmp(nombre_registro, "AX") == 0) return &(registros->AX);
+    if (strcmp(nombre_registro, "BX") == 0) return &(registros->BX);
+    if (strcmp(nombre_registro, "CX") == 0) return &(registros->CX);
+    if (strcmp(nombre_registro, "DX") == 0) return &(registros->DX);
+    
+    if (strcmp(nombre_registro, "EAX") == 0) return &(registros->EAX);
+    if (strcmp(nombre_registro, "EBX") == 0) return &(registros->EBX);
+    if (strcmp(nombre_registro, "ECX") == 0) return &(registros->ECX);
+    if (strcmp(nombre_registro, "EDX") == 0) return &(registros->EDX);
+    
+    if (strcmp(nombre_registro, "SI") == 0) return &(registros->SI);
+    if (strcmp(nombre_registro, "DI") == 0) return &(registros->DI);
+    if (strcmp(nombre_registro, "PC") == 0) return &(registros->PC);
+    
+    return NULL;
+}
+
+uint32_t leer_valor_registro(t_registros* registros, char* nombre_registro) {
+    void* reg_ptr = obtener_registro(registros, nombre_registro);
+    if (reg_ptr == NULL) return 0;
+    
+    if (es_registro_8bits(nombre_registro)) {
+        return (uint32_t)(*(uint8_t*)reg_ptr);
+    } else {
+        return *(uint32_t*)reg_ptr;
+    }
+}
+
 // NOOP
 void ejecutar_NOOP(t_cpu* cpu, t_contexto* ctx) {
     usleep(1000 * config_get_int_value(cpu->config, "INSTRUCTION_DELAY"));
@@ -11,21 +48,54 @@ void ejecutar_NOOP(t_cpu* cpu, t_contexto* ctx) {
 
 // SET
 void ejecutar_SET(t_registros* reg, char* registro_destino, uint32_t valor) {
+    void* reg_ptr = obtener_registro(reg, registro_destino);
+    if (reg_ptr != NULL) {
+        if (es_registro_8bits(registro_destino)) {
+            *(uint8_t*)reg_ptr = (uint8_t)valor;
+        } else {
+            *(uint32_t*)reg_ptr = valor;
+        }
+    }
     log_debug(cpu->logger, "Ejecutando SET: Asignando valor %u al registro %s", valor, registro_destino);
 }
 
 // SUM
 void ejecutar_SUM(t_registros* reg, char* registro_destino, char* registro_origen) {
+    uint32_t valor_origen = leer_valor_registro(reg, registro_origen);
+    void* reg_dest_ptr = obtener_registro(reg, registro_destino);
+    
+    if (reg_dest_ptr != NULL) {
+        if (es_registro_8bits(registro_destino)) {
+            *(uint8_t*)reg_dest_ptr += (uint8_t)valor_origen;
+        } else {
+            *(uint32_t*)reg_dest_ptr += valor_origen;
+        }
+    }
     log_debug(cpu->logger, "Ejecutando SUM: Sumando valor de registro %s al registro %s", registro_origen, registro_destino);
 }
 
 // SUB
 void ejecutar_SUB(t_registros* reg, char* registro_destino, char* registro_origen) {
-   log_debug(cpu->logger, "Ejecutando SUB: Restando valor de registro %s al registro %s", registro_origen, registro_destino);
+   uint32_t valor_origen = leer_valor_registro(reg, registro_origen);
+    void* reg_dest_ptr = obtener_registro(reg, registro_destino);
+    
+    if (reg_dest_ptr != NULL) {
+        if (es_registro_8bits(registro_destino)) {
+            *(uint8_t*)reg_dest_ptr -= (uint8_t)valor_origen;
+        } else {
+            *(uint32_t*)reg_dest_ptr -= valor_origen;
+        }
+    }
+    log_debug(cpu->logger, "Ejecutando SUB: Restando valor de registro %s al registro %s", registro_origen, registro_destino);
 }
 
 // JNZ
 void ejecutar_JNZ(t_registros* reg, char* registro_evaluado, uint32_t nueva_instruccion) {
+    uint32_t valor = leer_valor_registro(reg, registro_evaluado);
+    // Solo actualiza el PC si el registro evaluado es distinto de 0
+    if (valor != 0) {
+        reg->PC = nueva_instruccion;
+    }
     log_debug(cpu->logger, "Ejecutando JNZ: Evaluando registro %s para saltar a instrucción %u", registro_evaluado, nueva_instruccion);
 }
 
@@ -51,6 +121,30 @@ int ejecutar_COPY_MEM(t_cpu* cpu, t_contexto* ctx, char* registro_tamano) {
 }
 
 void ejecutar_SYSCALL(t_cpu* cpu, t_contexto* ctx, t_instruccion_decodificada instruccion){
-    //TODO
-    log_debug(cpu->logger, "Ejecutando SYSCALL: %s", instruccion.nombre_operacion);
+    log_info(cpu->logger, "Delegando SYSCALL %s al Kernel Scheduler", instruccion.nombre_operacion);
+
+    // se guarda el contexto en memoria
+    enviar_contexto_a_memoria(cpu, ctx);
+    
+    // avisamos al Scheduler que el proceso fue desalojado por una syscall
+    // Usamos el codigo PROCESO_DESALOJADO y enviamos la Syscall y sus parametros para que el Kernel la procese
+    t_paquete* paquete = crear_paquete(PROCESO_DESALOJADO, crear_buffer());
+    agregar_a_paquete(paquete, &(ctx->pid), sizeof(int));
+    agregar_a_paquete(paquete, instruccion.nombre_operacion, strlen(instruccion.nombre_operacion) + 1);
+    
+    // enviamos argumentos
+    if (instruccion.argumento_operando_destino) {
+        agregar_a_paquete(paquete, instruccion.argumento_operando_destino, strlen(instruccion.argumento_operando_destino) + 1);
+    } else {
+        agregar_a_paquete(paquete, "", 1);
+    }
+    
+    if (instruccion.argumento_operando_origen) {
+        agregar_a_paquete(paquete, instruccion.argumento_operando_origen, strlen(instruccion.argumento_operando_origen) + 1);
+    } else {
+        agregar_a_paquete(paquete, "", 1);
+    }
+
+    enviar_paquete(paquete, cpu->socket_kernel_scheduler, cpu->logger);
+    eliminar_paquete(paquete);
 }
--- cpu/src/instrucciones.h
+++ cpu/src/instrucciones.h
@@ -4,6 +4,10 @@
 #include "cpu.h"
 #include <stdint.h>
 
+//funciones auxiliares
+bool es_registro_8bits(char *nombre);
+void* obtener_registro(t_registros* reg, char* nombre_registro);
+uint32_t leer_valor_registro(t_registros* registros, char* nombre_registro);
 
 void ejecutar_NOOP(t_cpu* cpu, t_contexto* ctx);
 void ejecutar_SET(t_registros* reg, char* registro_destino, uint32_t valor);
@@ -15,7 +19,6 @@ void ejecutar_JNZ(t_registros* reg, char* registro_evaluado, uint32_t nueva_inst
 int ejecutar_MOV_IN(t_cpu* cpu, t_contexto* ctx, char* registro_datos);
 int ejecutar_MOV_OUT(t_cpu* cpu, t_contexto* ctx, char* registro_datos);
 int ejecutar_COPY_MEM(t_cpu* cpu, t_contexto* ctx, char* registro_tamano);
-int manejar_SYSCALL(t_cpu* cpu, t_contexto* ctx, char* operacion, char** parametros);
 
 //syscalls
 void ejecutar_SYSCALL(t_cpu* cpu, t_contexto* ctx, t_instruccion_decodificada instruccion);
